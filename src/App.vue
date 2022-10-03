93926ec06ca247ad8c4cfd06a88a290477cfc6bf
Jose Armando Paniagua Sandoval <66578250+JoseArmandoPaniaguaSandoval@users.noreply.github.com>
2022-10-03 09:00:24 -0500

    new

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mb-12">
+    <Header />
+    <hr>
+  </div>
+  
+  <div class="container">
+    <Formulario />
+  </div>
+  
+  
+</template>
+
+<script>
+  import Header from "./components/Header.vue"
+  import Formulario from "./components/Formulario.vue"
+  export default{
+    components:{
+      Header,
+      Formulario
+    },
+
+  };
+</script>
